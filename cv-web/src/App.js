3cfad825edea33b328def986b8b4b8af1c9a0069
daniMartinAlo <danielmartinalonso90@gmail.com>
2024-05-28 21:06:04 +0200

    add components and navbar

--- cv-web/src/App.js
+++ cv-web/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Link as ScrollLink, Element } from 'react-scroll';
+import Home from './components/Home';
+import Exp from './components/Exp';
+import Education from './components/Education';
+import Skills from './components/Skills';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex">
+      <nav className="d-none d-md-block bg-dark sidebar">
+        <div className="sidebar-sticky">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <ScrollLink className="nav-link text-white" to="home" smooth={true} duration={500}>
+                Inicio
+              </ScrollLink>
+            </li>
+            <li className="nav-item">
+              <ScrollLink className="nav-link text-white" to="Exp" smooth={true} duration={500}>
+                Experiencia laboral
+              </ScrollLink>
+            </li>
+            <li className="nav-item">
+              <ScrollLink className="nav-link text-white" to="educacion" smooth={true} duration={500}>
+                Educación
+              </ScrollLink>
+            </li>
+            <li className="nav-item">
+              <ScrollLink className="nav-link text-white" to="skills" smooth={true} duration={500}>
+                Habilidades
+              </ScrollLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container-fluid">
+        <div className="row">
+          <main className="col-md-9 ml-sm-auto col-lg-10 px-4">
+            <Element name="home">
+              <Home />
+            </Element>
+            <Element name="Exp">
+              <Exp />
+            </Element>
+            <Element name="educacion">
+              <Education />
+            </Element>
+            <Element name="skills">
+              <Skills />
+            </Element>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
--- cv-web/src/components/Home.js
+++ cv-web/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Home = () => {
+  // En esta página tendré que poner mi foto y un poco lo que hago y como me inicíe en esto
   return (
     <section id="home" className="my-5">
       <h1>Bienvenido a mi CV</h1>
